64d43b82051a532d633dc1b42ba2c0429ccffe4e
Martuluv <martuguada.n5@gmail.com>
2026-02-19 01:15:23 -0300

    Agrega registro/login y loading

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegistroForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("El nombre de usuario ya existe.")
+        return username
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views # Importa todas las vistas que ya tengo
+from .views import registro_usuario  # Importa la función de registro que cree
 
 urlpatterns = [
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
+    path('registro/', registro_usuario, name='registro'),  # <-- Aca es donde el formulario de alta se mostrará y se procesará
     path('home/', views.home, name='home'),
     
     path('buscar/', views.search, name='buscar'),
@@ -16,3 +18,9 @@ urlpatterns = [
 
     path('exit/', views.exit, name='exit'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Esto sirve las imágenes durante el desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
